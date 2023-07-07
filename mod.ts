b6d9cbfd55db444860ceb39a660e4c06aee6c72e
Garrett Cox <garrettjcox@gmail.com>
2023-07-07 11:53:05 -0500

    Support for some admin commands

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 import { writeAll } from "https://deno.land/std@0.192.0/streams/write_all.ts";
+import { readLines } from "https://deno.land/std@0.193.0/io/read_lines.ts";
 
 const decoder = new TextDecoder();
 const encoder = new TextEncoder();
@@ -7,7 +8,7 @@ type ClientData = Record<string, any>;
 
 interface BasePacket {
   clientId?: number;
-  roomId: string;
+  roomId?: string;
   quiet?: boolean;
   targetClientId?: number;
 }
@@ -22,12 +23,23 @@ interface AllClientDataPacket extends BasePacket {
   clients: ClientData[];
 }
 
+interface ServerMessagePacket extends BasePacket {
+  type: "SERVER_MESSAGE";
+  message: string;
+}
+
+interface DisableAnchorPacket extends BasePacket {
+  type: "DISABLE_ANCHOR";
+}
+
 interface OtherPackets extends BasePacket {
   type: "REQUEST_SAVE_STATE" | "PUSH_SAVE_STATE";
 }
 
 type Packet =
   | UpdateClientDataPacket
+  | DisableAnchorPacket
+  | ServerMessagePacket
   | AllClientDataPacket
   | OtherPackets;
 
@@ -44,7 +56,6 @@ class Server {
         try {
           const client = new Client(connection, this);
           this.clients.push(client);
-          this.log(`Client Count: ${this.clients.length}`);
         } catch (error) {
           this.log(`Error connecting client: ${error.message}`);
         }
@@ -57,7 +68,6 @@ class Server {
   removeClient(client: Client) {
     const index = this.clients.indexOf(client);
     this.clients.splice(index, 1);
-    this.log(`Client Count: ${this.clients.length}`);
   }
 
   getOrCreateRoom(id: string) {
@@ -68,14 +78,12 @@ class Server {
 
     const newRoom = new Room(id, this);
     this.rooms.push(newRoom);
-    this.log(`Room Count: ${this.rooms.length}`);
     return newRoom;
   }
 
   removeRoom(room: Room) {
     const index = this.rooms.indexOf(room);
     this.rooms.splice(index, 1);
-    this.log(`Room Count: ${this.rooms.length}`);
   }
 
   log(message: string) {
@@ -312,3 +320,125 @@ function findDelimiterIndex(data: Uint8Array): number {
 
 const server = new Server();
 server.start();
+
+function sendServerMessage(client: Client, message: string) {
+  return client.sendPacket({
+    type: "SERVER_MESSAGE",
+    message,
+  });
+}
+
+function sendDisable(client: Client, message: string) {
+  sendServerMessage(client, message)
+    .finally(() =>
+      client.sendPacket({
+        type: "DISABLE_ANCHOR",
+      })
+    );
+}
+
+async function stop(message = "Server restarting") {
+  await Promise.all(
+    server.clients.map((client) =>
+      sendServerMessage(client, message)
+        .finally(() => {
+          client.disconnect();
+        })
+    ),
+  );
+
+  Deno.exit();
+}
+
+(async () => {
+  try {
+    for await (const line of readLines(Deno.stdin)) {
+      const [command, ...args] = line.split(" ");
+
+      switch (command) {
+        default:
+        case "help": {
+          console.log(
+            `Available commands:
+  help: Show this help message
+  roomCount: Show the number of rooms
+  clientCount: Show the number of clients
+  list: List all rooms and clients
+  stop <message>: Stop the server
+  message <clientId> <message>: Send a message to a client
+  messageAll <message>: Send a message to all clients
+  disable <clientId> <message>: Disable anchor on a client
+  disableAll <message>: Disable anchor on all clients`,
+          );
+          break;
+        }
+        case "roomCount": {
+          console.log(`Room count: ${server.rooms.length}`);
+          break;
+        }
+        case "clientCount": {
+          console.log(`Client count: ${server.clients.length}`);
+          break;
+        }
+        case "list": {
+          for (const room of server.rooms) {
+            console.log(`Room ${room.id}:`);
+            for (const client of room.clients) {
+              console.log(
+                `  Client ${client.id}: ${JSON.stringify(client.data)}`,
+              );
+            }
+          }
+          break;
+        }
+        case "disable": {
+          const [clientId, ...messageParts] = args;
+          const message = messageParts.join(" ");
+          const client = server.clients.find((c) =>
+            c.id === parseInt(clientId, 10)
+          );
+          if (client) {
+            sendDisable(client, message);
+          } else {
+            console.log(`Client ${clientId} not found`);
+          }
+          break;
+        }
+        case "disableAll": {
+          const message = args.join(" ");
+          for (const client of server.clients) {
+            sendDisable(client, message);
+          }
+          break;
+        }
+        case "message": {
+          const [clientId, ...messageParts] = args;
+          const message = messageParts.join(" ");
+          const client = server.clients.find((c) =>
+            c.id === parseInt(clientId, 10)
+          );
+          if (client) {
+            sendServerMessage(client, message);
+          } else {
+            console.log(`Client ${clientId} not found`);
+          }
+          break;
+        }
+        case "messageAll": {
+          const message = args.join(" ");
+          for (const client of server.clients) {
+            sendServerMessage(client, message);
+          }
+          break;
+        }
+        case "stop": {
+          const message = args.join(" ");
+          stop(message);
+          break;
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error readingt from stdin: ", error.message);
+  }
+})();
