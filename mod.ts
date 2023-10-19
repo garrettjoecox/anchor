2e5e4d2ffa84d40ce95d1fb4b65b9482e8197a25
mattman107 <65982675+mattman107@users.noreply.github.com>
2023-10-19 16:01:54 -0400

    Update mod.ts
    Removed port comparison to null

--- mod.ts
+++ mod.ts
@@ -50,7 +50,7 @@ class Server {
 
   async start() {
     const mPort = Deno.args[0];
-    if (mPort == null || !Number.isInteger(mPort)) {
+    if (!Number.isInteger(mPort)) {
       this.listener = Deno.listen({ port: 43384 });
     } else {
       this.listener = Deno.listen({ port: mPort });
